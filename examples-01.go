a726e61d5817731edc785e30cf83f516777a6c8c
jaey <jaeylim.0.0@gmail.com>
2022-09-03 00:41:51 +0900

    second commit

--- examples-01.go
+++ examples-01.go
@@ -1,5 +1,5 @@
-//fmt package
-package main
+//01.single input examples
+/*package main
 import "fmt"
 
 func main() {
@@ -8,4 +8,17 @@ func main() {
 	fmt.Scanf("%s", &name)
 	fmt.Println("Hey there, ", name)
 }
+*/
 
+//02.multiple input
+package main
+import "fmt"
+
+func main() {
+	var name string
+	var is_muggle bool
+
+	fmt.Print("Enter your name & are you a muggle: ")
+	fmt.Scanf("%s %t",&name, &is_muggle)
+	fmt.Println(name, is_muggle)
+}
