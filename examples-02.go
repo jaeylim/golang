c322a430bb0bc34f5621c2d1f213290b5b8a2e11
jaey <jaeylim.0.0@gmail.com>
2022-09-04 23:57:23 +0900

    operators

--- examples-02.go
+++ examples-02.go
@@ -9,15 +9,26 @@ func main(){
 	fmt.Print(city == city2)
 }*/
 // 2. Arithmetic Operators
+
 // 3. Logical Operators
 // 3-1. &&
-package main
+/*package main
 import "fmt"
 
 func main(){
 	var x int = 10
 	fmt.Println((x < 100) && (x < 200))
 	fmt.Println((x < 300) && (x < 0))
+}*/
+//3-2. ||
+package main
+import "fmt"
+
+func main(){
+	var x int = 10
+	fmt.Println((x < 0) || (x < 200))
+	fmt.Println((x < 0) || (x > 200))
 }
 // 4. Assignment Operators
+
 // 5. Bitwise Operators
