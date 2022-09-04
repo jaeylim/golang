0367bd6147f32f8e67012eda2e06566a883ae4b9
jaey <jaeylim.0.0@gmail.com>
2022-09-04 23:53:53 +0900

    third commit

--- examples-02.go
+++ examples-02.go
@@ -1,14 +1,23 @@
 // operators
 // 1. Comparison Operators
-package main
+/*package main
 import "fmt"
 
 func main(){
 	var city string = "seoul"
 	var city2 string = "tokyo"
 	fmt.Print(city == city2)
-}
+}*/
 // 2. Arithmetic Operators
 // 3. Logical Operators
+// 3-1. &&
+package main
+import "fmt"
+
+func main(){
+	var x int = 10
+	fmt.Println((x < 100) && (x < 200))
+	fmt.Println((x < 300) && (x < 0))
+}
 // 4. Assignment Operators
 // 5. Bitwise Operators
