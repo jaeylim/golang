0427a41c9e0648459358dea3a0d518bae982bd43
jaey <jaeylim.0.0@gmail.com>
2022-09-04 22:11:36 +0900

    second commit

--- examples-02.go
+++ examples-02.go
@@ -1,5 +1,13 @@
 // operators
 // 1. Comparison Operators
+package main
+import "fmt"
+
+func main(){
+	var city string = "seoul"
+	var city2 string = "tokyo"
+	fmt.Print(city == city2)
+}
 // 2. Arithmetic Operators
 // 3. Logical Operators
 // 4. Assignment Operators
